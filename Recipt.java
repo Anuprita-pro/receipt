1bf4c0c8a0d8f2dcb5ed1315d3f221328636df9b
Anuprita Rajput <74887568+Anuprita-pro@users.noreply.github.com>
2022-02-01 09:38:25 +0530

    recipt purpose

---
+++ Recipt.java
@@ -0,0 +1,18 @@
+ String P_No= String.format(jTextField1.getText());
+        String P_Name = String.format(jTextField2.getText());
+        String  Tablets = String.format(jTextField3.getText());
+        String DOB = String.format(jTextField4.getText());
+        String P_Address = String.format(jTextField14.getText());
+        String DAUM = String.format(jTextField3.getText());
+        String Madication = String.format(jTextField7.getText());
+        String  Tablets_Name = String.format(jTextField1.getText());
+        String Dose = String.format(jTextField12.getText());
+        String Daily_Dose = String.format(jTextField6.getText());
+        String Exp_Date = String.format(jTextField10.getText());
+        String Issue_Date = String.format(jTextField13.getText());
+        String Bill = String.format(jTextField15.getText());
+        jTextArea1.append("\nP_No :"+P_No+"\n Patient Nmae :"+P_Name+"\nPatient Address :"
+	+P_Address+"\nDate Of Birth :"+DOB+"\nName of the Tablets :"+Tablets_Name+"\n No of Tablets :"
+	+Tablets+"\n Driving And Using Machine :"+DAUM+"\nHow to Used Madication :"+Madication+"\nDose(mg) :"
+	+Dose+"\nDaily_Dose  :"+Daily_Dose+"\nIssue Date  :"
+	+Issue_Date+"\nExp Date :"+Exp_Date+"\nBill Rs. :"+Bill+"\n\t Thank you!");
